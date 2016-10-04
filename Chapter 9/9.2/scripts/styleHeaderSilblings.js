05caff5b2d6a49e07ea9de9cc0798b3416cc10de
StYaphet <947670170@qq.com>
2016-10-04 21:26:13 +0800

    第八章finish、第九章init

---
+++ Chapter 9/9.2/scripts/styleHeaderSilblings.js
@@ -0,0 +1,35 @@
+function getNextElement(node) {
+	if(node.nodeType == 1) {
+		return node;
+	}
+	if(node.nextSibling) {
+		return getNextElement(node.nextSibling);
+	}
+	return null;
+}
+
+function styleHeaderSibling() {
+	if(!document.getElementsByTagName)	return false;
+	var headers = document.getElementsByTagName("h1");
+	var elem;
+	for(var i=0;i<headers.length;i++) {
+		elem = getNextElement(headers[i].nextSibling);
+		//  style对象的属性值必须放在括号里，单引号、双引号均可
+		elem.style.fontWeight = "bold";
+		elem.style.fontSize = "1.2em";
+	}
+}
+
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if(typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(styleHeaderSibling);
